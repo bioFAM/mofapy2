4c1e2956416ffbdc92499612c3ddc756c88662ab
rargelaguet <ricard.argelaguet@gmail.com>
2019-07-18 11:48:55 +0200

    rename from biofam to mofa2 in the R package

---
+++ core/nodes/__init__.py
@@ -0,0 +1,10 @@
+from .Alpha_nodes import AlphaW_Node, AlphaZ_Node
+from .Tau_nodes import TauD_Node
+from .Theta_nodes import ThetaW_Node, ThetaZ_Node
+from .W_nodes import W_Node, SW_Node
+from .Y_nodes import Y_Node
+from .Z_nodes import Z_Node, SZ_Node
+from .multiview_nodes import *
+from .basic_nodes import *
+from .nongaussian_nodes import *
+from .variational_nodes import *
