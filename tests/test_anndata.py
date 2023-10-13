Fix precision in tests

tests/test_anndata.py
@@ -60,11 +60,11 @@ class TestAnnData:
         assert all(adata.obs.group.values == adata.obs.true_group.values)
 
         for sample, value in (
-            ("sample0_groupA", 0.1459154),
-            ("sample7_groupB", -0.1822545),
+            ("sample0_groupA", 0.145915),
+            ("sample7_groupB", -0.182254),
         ):
             si = np.where(adata.obs_names == sample)[0][0]
-            assert adata.obsm["X_mofa"][si, 0] == pytest.approx(value)
+            assert adata.obsm["X_mofa"][si, 0] == pytest.approx(value, 1e-5)
 
 
 if __name__ == "__main__":